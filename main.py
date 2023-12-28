32089a2dd7c8abc9347e64ded5f1dd4993618484
bendamian <badamian@gmail.com>
2023-12-28 22:20:16 +0000

    add canvas

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tkinter import *
+from tkinter import ttk
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -11,8 +13,23 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
+
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+# create a window
+window = Tk()
+window.title('Pomodoro')
+# window.geometry('220x224')
+window.config(padx=100, pady=50)
+
+canvas = Canvas(width=220, height=224)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(110, 112, image=tomato_img)
+canvas.create_text(103,112,text="00.00")
+canvas.pack()
+
+# run
+window.mainloop()
