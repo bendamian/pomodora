e84830a131bbbb84c68ed89bde458e1d516b800f
bendamian <badamian@gmail.com>
2023-12-30 21:38:20 +0000

    add timer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 import ttkbootstrap as ttk
 
 # from tkinter import ttk
@@ -13,12 +14,23 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+
 # ---------------------------- TIMER RESET ------------------------------- #
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    count_down(5 * 60)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 # create a window
@@ -28,15 +40,15 @@ window.title('Pomodoro')
 window.config(padx=100, pady=50, bg=YELLOW)
 
 # widgets
-label1 = Label(window, text='Timer', fg=GREEN, bg=YELLOW , font=(FONT_NAME, 50))
-label2 = Label(window, text='✔', fg=GREEN,bg=YELLOW,font=(FONT_NAME, 25))
-button1 = Button(window, text='Start')
+label1 = Label(window, text='Timer', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+label2 = Label(window, text='✔', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 25))
+button1 = Button(window, text='Start', command=start_timer)
 button2 = Button(window, text='Reset')
 
 canvas = Canvas(width=220, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(110, 112, image=tomato_img)
-canvas.create_text(103, 130, text="00.00", fill="white", font=(FONT_NAME, 35, 'bold'))
+timer_text = canvas.create_text(103, 130, text="00.00", fill="white", font=(FONT_NAME, 35, 'bold'))
 
 # define a grid
 window.columnconfigure(0, weight=1)
